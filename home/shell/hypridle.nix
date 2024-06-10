8e4acf81cab5734265b760a56d7abc6459dc0f68
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-06-10 16:21:50 +0200

    hypridle suspend and screen off

--- home/shell/hypridle.nix
+++ home/shell/hypridle.nix
@@ -11,14 +11,32 @@ in
   config = lib.mkIf cfg.enable {
     services.hypridle = {
       enable = true;
+
       settings = {
         general = {
           lock_cmd = "pidof hyprlock || hyprlock";
         };
-        listener = [{
-          timeout = 500;
-          on-timeout = "loginctl lock-session";
-        }];
+
+        listener = [
+          # Lockscreen
+          {
+            timeout = 300;
+            on-timeout = "loginctl lock-session";
+          }
+
+          # Screen off
+          {
+            timeout = 330;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+
+          # Suspend
+          {
+            timeout = 600;
+            on-timeout = "systemctl suspend";
+          }
+        ];
       };
     };
   };
