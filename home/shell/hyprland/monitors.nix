5dd9d2c407dc58c4d00af431e5363e4f11039a1e
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-07-25 22:38:20 +0200

    set work laptop hidpi res scale to 2

--- home/shell/hyprland/monitors.nix
+++ home/shell/hyprland/monitors.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.extraConfig = ''
     monitor=Unknown-1,disable
-    monitor=eDP-1,3456x2160,auto,1
+    monitor=eDP-1,3456x2160,auto,2
     monitor=,preferred,auto,auto
   '';
 }
