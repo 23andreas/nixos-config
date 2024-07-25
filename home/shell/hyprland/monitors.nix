73a98308bb25012f1b74c733e23b7cfeba70b15a
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-07-25 11:25:13 +0200

    set work laptop built in monitor resolution to 3456x2160

--- home/shell/hyprland/monitors.nix
+++ home/shell/hyprland/monitors.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.extraConfig = ''
     monitor=Unknown-1,disable
-    monitor=eDP-1,highres,auto,auto
+    monitor=eDP-1,3456x2160,auto,1
     monitor=,preferred,auto,auto
   '';
 }
