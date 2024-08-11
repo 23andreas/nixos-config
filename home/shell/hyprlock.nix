b0aace37f88b7018770a3f31ac120443b42f9ef9
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-08-11 23:37:31 +0200

    change hyprlock input transparency

--- home/shell/hyprlock.nix
+++ home/shell/hyprlock.nix
@@ -41,8 +41,8 @@ in
           dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
           dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
           dots_center = true;
-          outer_color = "rgba(25, 23, 36, 0.7)";
-          inner_color = "rgba(25, 23, 36, 0.7)";
+          outer_color = "rgba(25, 23, 36, 0)";
+          inner_color = "rgba(25, 23, 36, 0.4)";
           font_color = "rgb(200, 200, 200)";
           fade_on_empty = true;
           rounding = 5;
