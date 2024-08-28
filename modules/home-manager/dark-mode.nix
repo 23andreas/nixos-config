91e8b9314e5896db7df8fe81a48ab7c023b7aad1
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-08-28 09:36:43 +0200

    add dark mode module

--- modules/home-manager/dark-mode.nix
+++ modules/home-manager/dark-mode.nix
@@ -2,31 +2,33 @@
 
 # TODO: Fix hardcoded user here
 {
-  home-manager.users.andreas = {
-    dconf.settings = {
-      "org/gnome/desktop/background" = {
-        picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
-      };
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
     };
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Adwaita-dark";
-        package = pkgs.gnome.gnome-themes-extra;
-      };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
     };
-
-    # Wayland, X, etc. support for session vars
-    # systemd.user.sessionVariables = config.home-manager.users.justinas.home.sessionVariables;
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.gnome-themes-extra;
+    };
+  };
+
+  # Wayland, X, etc. support for session vars
+  # systemd.user.sessionVariables = config.home-manager.users.justinas.home.sessionVariables;
   qt = {
     enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
+    # platformTheme = "gnome";
+    platformTheme.name = "kvantum";
+    style = {
+      name = "kvantum";
+      package = pkgs.qt6Packages.qtstyleplugin-kvantum;
+    };
   };
 }
+
