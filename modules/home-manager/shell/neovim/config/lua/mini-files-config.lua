ccf4f617a1608d357b59177a604d0fbb443cf239
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-10-28 07:50:54 +0100

    update mini files config

--- modules/home-manager/shell/neovim/config/lua/mini-files-config.lua
+++ modules/home-manager/shell/neovim/config/lua/mini-files-config.lua
@@ -1,17 +1,22 @@
 -- Load and configure mini.files
-local mini_files = require('mini.files')
+local miniFiles = require('mini.files')
 
-mini_files.setup({
+miniFiles.setup({
   windows = {
     preview = true, -- Show file preview window
   },
   mappings = {
-    go_in_plus = '<CR>', -- Enter directory
-    go_in = '<Tab>',     -- Open file or folder
-    go_out = '-',        -- Go up one directory
-    reset = '<Esc>',     -- Reset to the root directory
+    -- go_in_plus = '<CR>', -- Enter directory
+    -- go_in = '<Tab>',     -- Open file or folder
+    -- go_out = '-',        -- Go up one directory
+    -- reset = '<Esc>',     -- Reset to the root directory
   },
 })
 
 -- Keybinding to open mini.files with <leader>e
-vim.keymap.set('n', '<leader>e', mini_files.open, { desc = 'Open files' })
+-- vim.keymap.set('n', '<leader>e', mini_files.open, { desc = 'Open files' })
+
+vim.keymap.set("n", "<leader>e", function()
+  miniFiles.open(vim.api.nvim_buf_get_name(0), false)
+  miniFiles.reveal_cwd()
+end)
