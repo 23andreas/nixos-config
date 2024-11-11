229df9d21469b2a20d8494a06400d217e79b3375
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-11-11 16:27:04 +0100

    update twilight settings

--- modules/home-manager/shell/neovim/nvim/lua/plugins/twilight.lua
+++ modules/home-manager/shell/neovim/nvim/lua/plugins/twilight.lua
@@ -2,9 +2,13 @@ return {
   "folke/twilight.nvim",
   opts = {
     treesitter = true,
+    context = 10,
     dimming = {
-      alpha = 0.2,
-      term_bg = "#FFF8F5"
+      alpha = 0.25,
+      -- color = {
+      --   "Normal", "#ffffff"
+      -- }
+      -- term_bg = "#1F2733"
     }
   },
   keys = {
