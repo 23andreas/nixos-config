781dc3e169cc0b5cce24822eb5132ed8d3c92389
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-11-11 16:25:23 +0100

    neogit toggle keybind

--- modules/home-manager/shell/neovim/nvim/lua/plugins/neogit.lua
+++ modules/home-manager/shell/neovim/nvim/lua/plugins/neogit.lua
@@ -1,24 +1,32 @@
-return
-{
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-    "nvim-telescope/telescope.nvim", -- optional
-  },
-  event = "VeryLazy",
-  opts = {
-    telescope_sorter = function()
-      return require("telescope").extensions.fzf.native_fzf_sorter()
-    end
-  },
-  keys = {
-    {
-      "<leader>hh",
-      mode = { "n", "v" },
-      function() require("neogit").open({ kind = "auto" }) end,
-      desc = "Neogit"
-    }
-  },
-  config = true
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    event = "VeryLazy",
+    opts = {
+      telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+      end
+    },
+    keys = {
+      {
+        "<leader>hh",
+        mode = { "n", "v" },
+        function()
+          local neogit = require('neogit')
+          if vim.fn.bufwinnr('NeogitStatus') ~= -1 then
+            neogit.close()
+          else
+            neogit.open({ kind = "auto" })
+          end
+        end,
+        desc = "Neogit"
+      }
+    },
+    config = true
+  }
 }
