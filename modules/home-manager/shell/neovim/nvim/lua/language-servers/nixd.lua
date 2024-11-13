9eee425a0e91168fa31500a5701e19ba9d5fa3f6
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-11-13 08:26:18 +0100

    update nvim

---
+++ modules/home-manager/shell/neovim/nvim/lua/language-servers/nixd.lua
@@ -0,0 +1,25 @@
+local lspconfig = require("lspconfig")
+
+return function(on_attach)
+  lspconfig.nixd.setup {
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+    end,
+    settings = {
+      nixd = {
+        formatting = {
+          command = "nixfmt --no-expand-braces"
+        },
+        options = {
+          enable = true,
+          target = {
+            args = {},
+            installable = "",
+            nixPath = "",
+          },
+        },
+      },
+    },
+    filetypes = { "nix" },
+  }
+end
