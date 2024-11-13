69dc37d9f595ab3aad396519d2e6ccd52e0c374d
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-11-13 08:37:36 +0100

    rename autocmds

--- modules/home-manager/shell/neovim/nvim/lua/config/autocmds/init.lua
+++ modules/home-manager/shell/neovim/nvim/lua/config/autocmds/init.lua
@@ -1,4 +1,4 @@
 require("config.autocmds.autoresize")
 require("config.autocmds.buffer-restore-last-position")
 require("config.autocmds.highlight-yank")
-require("config.autocmds.line_numbers_toggle")
+require("config.autocmds.line-numbers-toggle")
