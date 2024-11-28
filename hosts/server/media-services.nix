241f0015ae341bfbec03dad5401cfe014f52704b
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-11-28 19:33:15 +0100

    updates

--- hosts/server/media-services.nix
+++ hosts/server/media-services.nix
@@ -1,80 +1,84 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 {
   imports = [
     ../../modules/nix/modules/qbittorrent-nox.nix
     ../../modules/nix/modules/torrent-extractor.nix
   ];
 
-  users.groups.media = {};
+  users.groups.media = { };
 
-  networking.firewall.allowedTCPPorts = [ 7219 39603 ];
+  networking.firewall.allowedTCPPorts = [
+    7219
+    39603
+  ];
   services = {
-    qbittorrent = let
-      qbittorrentPort = 39603;
-    in
-    {
-      enable = true;
-      port = 7219;
-      settings = {
-        LegalNotice = {
-          Accepted = true;
-        };
-        BitTorrent = {
-          "Session\\PieceExtentAffinity" = true;
-          "Session\\Port" = qbittorrentPort;
-        };
-        Preferences = {
-          "Advanced\\AnonymousMode" = false;
-          "Advanced\\osCache" = false;
-          "Bittorrent\\Encryption" = 0;
-          "Bittorrent\\LSD" = false;
-          "Bittorrent\\MaxConnecs" = 10000;
-          "Bittorrent\\MaxConnecsPerTorrent" = 1000;
-          "Bittorrent\\MaxRatio" = 1000;
-          "Bittorrent\\MaxUploads" = 1000;
-          "Bittorrent\\MaxUploadsPerTorrent" = 100;
-          "Connection\\GlobalUPLimit" = 8192;
-          "Connection\\PortRangeMin" = qbittorrentPort;
-          "Connection\\ResolvePeerCountries" = true;
-          "Connection\\UPnP" = false;
-          "Downloads\\SavePath" = "/media/torrent/downloads";
-          "Downloads\\StartInPause" = false;
-          "Downloads\\RunExternalProgramEnabled" = true;
-          "Downloads\\RunExternalProgram" = "torrent-extractor \"%F\"";
-          "Queueing\\MaxActiveDownloads" = 1;
-          "Queueing\\MaxActiveTorrents" = 1000;
-          "Queueing\\MaxActiveUploads" = 1000;
-          "Queueing\\QueueingEnabled" = true;
-          "WebUI\\AuthSubnetWhitelist" = "192.168.1.0/24";
-          "WebUI\\AuthSubnetWhitelistEnabled" = true;
-          "WebUI\\LocalHostAuth" = false;
-          "WebUI\\Username" = "admin";
-          "WebUI\\UseUPnP"= false;
-          "WebUI\\Address" = "0.0.0.0";
-        };
-      };
-      user = "qbittorrent";
-      group = "media";
-    };
-    plex = let
-      plexLatest = pkgs.plex.override {
-        plexRaw = pkgs.plexRaw.overrideAttrs(old: rec {
-          version = "1.41.1.9057-af5eaea7a";
-          src = pkgs.fetchurl {
-
-            url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-            sha256 = "sha256-A4OL70fu4tWnqTc5JvUl2I7p+p8aMYWk+B5HWdlFKpQ=";
+    qbittorrent =
+      let
+        qbittorrentPort = 39603;
+      in
+      {
+        enable = true;
+        port = 7219;
+        settings = {
+          LegalNotice = {
+            Accepted = true;
           };
-        });
+          BitTorrent = {
+            "Session\\PieceExtentAffinity" = true;
+            "Session\\Port" = qbittorrentPort;
+          };
+          Preferences = {
+            "Advanced\\AnonymousMode" = false;
+            "Advanced\\osCache" = false;
+            "Bittorrent\\Encryption" = 0;
+            "Bittorrent\\LSD" = false;
+            "Bittorrent\\MaxConnecs" = 10000;
+            "Bittorrent\\MaxConnecsPerTorrent" = 1000;
+            "Bittorrent\\MaxRatio" = 1000;
+            "Bittorrent\\MaxUploads" = 1000;
+            "Bittorrent\\MaxUploadsPerTorrent" = 100;
+            "Connection\\GlobalUPLimit" = 8192;
+            "Connection\\PortRangeMin" = qbittorrentPort;
+            "Connection\\ResolvePeerCountries" = true;
+            "Connection\\UPnP" = false;
+            "Downloads\\SavePath" = "/media/torrent/downloads";
+            "Downloads\\StartInPause" = false;
+            "Downloads\\RunExternalProgramEnabled" = true;
+            "Downloads\\RunExternalProgram" = "torrent-extractor \"%F\"";
+            "Queueing\\MaxActiveDownloads" = 1;
+            "Queueing\\MaxActiveTorrents" = 1000;
+            "Queueing\\MaxActiveUploads" = 1000;
+            "Queueing\\QueueingEnabled" = true;
+            "WebUI\\AuthSubnetWhitelist" = "192.168.1.0/24";
+            "WebUI\\AuthSubnetWhitelistEnabled" = true;
+            "WebUI\\LocalHostAuth" = false;
+            "WebUI\\Username" = "admin";
+            "WebUI\\UseUPnP" = false;
+            "WebUI\\Address" = "0.0.0.0";
+          };
+        };
+        user = "qbittorrent";
+        group = "media";
       };
-    in
-    {
-      enable = true;
-      group = "media";
-      openFirewall = true;
-      package = plexLatest;
-      accelerationDevices = [ "/dev/dri/renderD128" ];
-    };
+    plex =
+      let
+        plexLatest = pkgs.plex.override {
+          plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
+            version = "1.41.2.9200-c6bbc1b53";
+            src = pkgs.fetchurl {
+              url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
+              sha256 = "sha256-HmgtnUsDzRIUThYdlZIzhiU02n9jSU7wtwnEA0+r1iQ=";
+            };
+          });
+        };
+      in
+      {
+        enable = true;
+        group = "media";
+        openFirewall = true;
+        package = plexLatest;
+        accelerationDevices = [ "/dev/dri/renderD128" ];
+      };
     sonarr = {
       enable = true;
       group = "media";
@@ -94,4 +98,3 @@
     };
   };
 }
-
