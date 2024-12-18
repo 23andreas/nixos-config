b86f564c0eff403588c04b1a48d1a2853a72073c
Andreas Skønberg <andreas.skonberg@gmail.com>
2024-12-18 15:16:53 +0100

    remove cmp from treesitter autopairs

--- modules/home-manager/shell/neovim/nvim/lua/plugins/treesitter.lua
+++ modules/home-manager/shell/neovim/nvim/lua/plugins/treesitter.lua
@@ -54,12 +54,13 @@ return {
     config = function(_, opts)
       require('nvim-autopairs').setup(opts)
 
-      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-      local cmp = require('cmp')
-      cmp.event:on(
-        "confirm_done",
-        cmp_autopairs.on_confirm_done()
-      )
+      -- TODO fix this with blink
+      -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      -- local cmp = require('cmp')
+      -- cmp.event:on(
+      --   "confirm_done",
+      --   cmp_autopairs.on_confirm_done()
+      -- )
     end
   },
   {
