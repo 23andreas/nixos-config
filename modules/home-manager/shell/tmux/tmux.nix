49b6a3aaa9355acaa97e030fb423792e107c6832
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-02-17 09:22:16 +0100

    disable tmux resurrect and continuum

--- modules/home-manager/shell/tmux/tmux.nix
+++ modules/home-manager/shell/tmux/tmux.nix
@@ -34,10 +34,6 @@ in
         bind k select-pane -U
         bind l select-pane -R
 
-        resurrect_dir="$HOME/.tmux/resurrect"
-        set -g @resurrect-dir $resurrect_dir
-        set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" $target | sponge $target'
-
         set-option -g status-interval 5
         # set-option -g automatic-rename on
         # set-option -g automatic-rename-format '#{b:pane_current_command} (#{b:pane_current_path})'
@@ -86,26 +82,26 @@ in
         }
         # tmuxPlugins.notify
         # tmuxPlugins.tmux-window-name
-        {
-          plugin = tmuxPlugins.resurrect;
-          extraConfig = ''
-            set -g @resurrect-strategy-nvim 'session'
-            set -g @resurrect-capture-pane-contents 'on'
-            set -g @resurrect-processes 'lazygit'
-          '';
-        }
+        # {
+        #   plugin = tmuxPlugins.resurrect;
+        #   extraConfig = ''
+        #     set -g @resurrect-strategy-nvim 'session'
+        #     set -g @resurrect-capture-pane-contents 'on'
+        #     set -g @resurrect-processes 'lazygit'
+        #   '';
+        # }
         # {
         # plugin = tmuxPlugins.catppuccin;
         # extraConfig = ''
         #   set -g @catppuccin_window_default_text '#{b:pane_current_command} (#{b:pane_current_path})'
         # '';
         # }
-        {
-          plugin = tmuxPlugins.continuum;
-          extraConfig = ''
-            set -g @continuum-restore 'on'
-          '';
-        }
+        # {
+        #   plugin = tmuxPlugins.continuum;
+        #   extraConfig = ''
+        #     set -g @continuum-restore 'on'
+        #   '';
+        # }
       ];
     };
   };
