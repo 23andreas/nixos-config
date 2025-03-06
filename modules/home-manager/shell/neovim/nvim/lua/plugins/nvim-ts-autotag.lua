7105b798e4b1b37c1f44c610c55fd4df7e32ab98
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-03-06 08:08:26 +0100

    update nvim ts autotag

--- modules/home-manager/shell/neovim/nvim/lua/plugins/nvim-ts-autotag.lua
+++ modules/home-manager/shell/neovim/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -2,9 +2,9 @@ return {
   'windwp/nvim-ts-autotag',
   event = "InsertEnter",
   opts = {
-    enable_close = true,
-    enable_rename = true,
-    enable_close_on_slash = true,
+    enable_close = true,          -- Auto close tags
+    enable_rename = true,         -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
     -- filetypes = {
     --   "html", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte", "vue", "xml", "tsx", "jsx"
     -- }
