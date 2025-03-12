e7c346064cf78e25c0a8053585637e1fd003b03a
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-03-12 20:47:46 +0100

    add terminal navigate windows keybindings

--- modules/home-manager/shell/neovim/nvim/lua/config/keymap.lua
+++ modules/home-manager/shell/neovim/nvim/lua/config/keymap.lua
@@ -32,6 +32,12 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
 
+-- Navigate windows in terminal mode as well
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', { silent = true })
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', { silent = true })
+vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', { silent = true })
+vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', { silent = true })
+
 -- Resize windows
 vim.keymap.set("n", "<C-M-h>", ":vertical resize -10<CR>", { silent = true })
 vim.keymap.set("n", "<C-M-l>", ":vertical resize +10<CR>", { silent = true })
