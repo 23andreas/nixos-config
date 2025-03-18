7f6bc3bcac6749c1e5d92046785596a2e718bd82
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-03-18 15:09:17 +0100

    show rec in lualine when recording macro

--- modules/home-manager/shell/neovim/nvim/lua/plugins/lua-line.lua
+++ modules/home-manager/shell/neovim/nvim/lua/plugins/lua-line.lua
@@ -3,32 +3,35 @@ return {
   event = "VeryLazy",
   opts = {
     options = {
-      section_separators = {
-        left = '',
-      },
-      component_separators = {
-        left = '',
-        right = ''
-      }
+      section_separators = { left = '' },
+      component_separators = { left = '', right = '' }
     },
     sections = {
       lualine_a = {},
-      lualine_b = { "branch", "diff" },
+      lualine_b = {
+        {
+          function()
+            local reg = vim.fn.reg_recording()
+            if reg == "" then
+              return ""
+            else
+              return " REC @" .. reg
+            end
+          end,
+        },
+        "branch",
+        "diff"
+      },
       lualine_c = {
         {
           'filename',
           path = 4,
-        }
+        },
       },
       lualine_x = { "searchcount", "diagnostics" },
       lualine_y = {},
       lualine_z = {}
     },
-    extensions = {
-      'lazy',
-      'oil',
-      'trouble'
-    },
+    extensions = { 'lazy', 'oil', 'trouble' },
   }
 }
-
