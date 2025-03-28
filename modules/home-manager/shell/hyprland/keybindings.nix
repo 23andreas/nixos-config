537ff0ec312df4934f9aabd251ec970be583edf4
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-03-28 14:06:55 +0100

    clean up hyprland keybindings file

--- modules/home-manager/shell/hyprland/keybindings.nix
+++ modules/home-manager/shell/hyprland/keybindings.nix
@@ -1,16 +1,17 @@
 { config, ... }:
 let
   playerctl = "${config.services.playerctld.package}/bin/playerctl";
+  volumectl = "volumectl";
+  lightctl = "lightctl";
+  uwsm = "uwsm app --";
+  screenshotsDir = "~/Pictures/Screenshots";
 in
 {
   wayland.windowManager.hyprland = {
     extraConfig = ''
       bind=$mod, R, submap, resize
 
-      # will start a submap called "resize"
       submap=resize
-
-      # sets repeatable binds for resizing the active window
       binde=,h,resizeactive,-200 0
       binde=,j,resizeactive,0 200
       binde=,k,resizeactive,0 -200
@@ -21,11 +22,9 @@ in
       binde=$mod, k, resizeactive, 0 -50
       binde=$mod, l, resizeactive, 50 0
 
-      # use reset to go back to the global submap
       bind=,escape,submap,reset
       bind=$mod, R, submap, reset
 
-      # will reset the submap, meaning end the current one and return to the global one
       submap=reset
     '';
 
@@ -39,87 +38,54 @@ in
         "$mod, mouse:273, resizewindow"
       ];
 
-      # l = locked (works with lockscreen etc)
       bindl = [
-        # media controls
         ", XF86AudioPlay, exec, ${playerctl} play-pause"
-
-        # holding mod with media controls targets spotify
         "$mod, XF86AudioPlay, exec, ${playerctl} --player=spotify play-pause"
-
-        # volume
-        ", XF86AudioMute, exec, volumectl toggle-mute"
-        ", XF86AudioMicMute, exec, volumectl -m toggle-mute"
-        # ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        # ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ", XF86AudioMute, exec, ${volumectl} toggle-mute"
+        ", XF86AudioMicMute, exec, ${volumectl} -m toggle-mute"
       ];
 
-      # le = locked, repeat
       bindle = [
-        # volume
-        ", XF86AudioRaiseVolume, exec, volumectl -u up"
-        ", XF86AudioLowerVolume, exec, volumectl -u down"
-        # ", XF86AudioRaiseVolume, exec, wpctl set-volume -l '1.0' @DEFAULT_AUDIO_SINK@ 6%+"
-        # ", XF86AudioLowerVolume, exec, wpctl set-volume -l '1.0' @DEFAULT_AUDIO_SINK@ 6%-"
-
-        # Prev, Next
+        ", XF86AudioRaiseVolume, exec, ${volumectl} -u up"
+        ", XF86AudioLowerVolume, exec, ${volumectl} -u down"
         ", XF86AudioPrev, exec, ${playerctl} previous"
         ", XF86AudioNext, exec, ${playerctl} next"
-        # holding mod with media controls targets spotify
         "$mod, XF86AudioPrev, exec, ${playerctl} --player=spotify previous"
         "$mod, XF86AudioNext, exec, ${playerctl} --player=spotify next"
-
-        # Brightness
-        ", XF86MonBrightnessUp, exec, lightctl up"
-        ", XF86MonBrightnessDown, exec, lightctl down"
+        ", XF86MonBrightnessUp, exec, ${lightctl} up"
+        ", XF86MonBrightnessDown, exec, ${lightctl} down"
       ];
 
       bind = [
         "$mod, Space, exec, pgrep walker && pkill walker || walker"
-        "$mod, Return, exec, uwsm app -- kitty"
-        "$mod, semicolon, exec, uwsm app -- google-chrome-stable --profile-directory='Default'"
-        "$mod+Alt, semicolon, exec, uwsm app -- google-chrome-stable --profile-directory='Profile 1'"
-
-        # Powermenu, Exit
+        "$mod, Return, exec, ${uwsm} kitty"
+        "$mod, semicolon, exec, ${uwsm} google-chrome-stable --profile-directory='Default'"
+        "$mod+Alt, semicolon, exec, ${uwsm} google-chrome-stable --profile-directory='Profile 1'"
         "$mod, Q, exec, ~/.local/share/powermenu.sh"
         "$mod+Alt, Q, exit,"
-
-        # Switch layout
         "$mod, M, exec, hyprctl keyword general:layout \"master\""
         "$mod, D, exec, hyprctl keyword general:layout \"dwindle\""
-
-        # Screen capture
-        ", Print, exec, grimblast --freeze save output - | satty --filename - --output-filename ~/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png"
-        "SHIFT, Print, exec, grimblast --freeze save area - | satty --filename - --output-filename ~/Pictures/Screenshots/$(date '+%Y%m%d-%H:%M:%S').png"
+        ", Print, exec, grimblast --freeze save output - | satty --filename - --output-filename ${screenshotsDir}/$(date '+%Y%m%d-%H:%M:%S').png"
+        "SHIFT, Print, exec, grimblast --freeze save area - | satty --filename - --output-filename ${screenshotsDir}/$(date '+%Y%m%d-%H:%M:%S').png"
         "$mod, Print, exec, ~/.local/share/recordmenu.sh"
-
         "$mod, V, togglefloating,"
         "$mod, B, pin,"
         "$mod, F, fullscreen"
         "$mod, C, killactive,"
-
         "$mod, P, pseudo, # dwindle"
         "$mod, O, togglesplit, # dwindle"
-
-        #GROUPS
         "$mod, T, togglegroup,"
         "$mod, Y, changegroupactive, f"
         "$mod, U, moveoutofgroup,"
         "$mod, I, moveintogroup, r"
-
-        # Move focus with mainMod + arrow keys
         "$mod, h, movefocus, l"
         "$mod, j, movefocus, d"
         "$mod, k, movefocus, u"
         "$mod, l, movefocus, r"
-
-        # Move windows
         "$mod+Alt, h, movewindow, l"
         "$mod+Alt, j, movewindow, u"
         "$mod+Alt, k, movewindow, d"
         "$mod+Alt, l, movewindow, r"
-
-        # Switch workspaces with mainMod + [0-9]
         "$mod, 1, workspace, 1"
         "$mod, 2, workspace, 2"
         "$mod, 3, workspace, 3"
@@ -130,8 +96,6 @@ in
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
-
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
         "$mod+Alt, 1, movetoworkspacesilent, 1"
         "$mod+Alt, 2, movetoworkspacesilent, 2"
         "$mod+Alt, 3, movetoworkspacesilent, 3"
@@ -142,12 +106,8 @@ in
         "$mod+Alt, 8, movetoworkspacesilent, 8"
         "$mod+Alt, 9, movetoworkspacesilent, 9"
         "$mod+Alt, 0, movetoworkspacesilent, 10"
-
-        # Scratchpad
         "$mod, S, togglespecialworkspace, magic"
         "$mod+Alt, S, movetoworkspace, special:magic"
-
-        # Scroll through existing workspaces with mainMod + scroll
         "$mod, mouse_down, workspace, e+1"
         "$mod, mouse_up, workspace, e-1"
       ];
