ae0eb7d65d64cdc5202c5f74bbdccdb4b68ad982
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-04-25 07:41:09 +0200

    update terminal settings

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -1,20 +1,28 @@
-{ config, users, lib, ...}:
+{
+  config,
+  users,
+  lib,
+  ...
+}:
 
 let
   username = config.home.username;
   envVarFiles = users.${username}.envVarFiles;
-in {
+in
+{
   programs.fish = {
     enable = true;
     shellInit = ''
-      ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: path: ''
-        set -x ${name} (cat ${path})
-      '') envVarFiles)}
+      ${lib.concatStringsSep "\n" (
+        lib.mapAttrsToList (name: path: ''
+          set -x ${name} (cat ${path})
+        '') envVarFiles
+      )}
     '';
   };
 
   programs.atuin = {
-    enable = true;
+    enable = false;
     enableFishIntegration = true;
     flags = [
       "--disable-up-arrow"
@@ -26,12 +34,11 @@ in {
     enableFishIntegration = true;
   };
 
-  programs.navi = {
-    enable = true;
-    enableFishIntegration = true;
-  };
+  # programs.navi = {
+  #   enable = true;
+  #   enableFishIntegration = true;
+  # };
 
   programs.starship.enable = true;
   programs.starship.enableFishIntegration = true;
 }
-
