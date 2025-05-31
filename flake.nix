b4732e69893271bcd958113211e4ba9271582dd7
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-05-31 21:32:07 +0200

    update nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,15 +1,11 @@
 {
   description = "NixOS Config";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # hyprpaper = {
-    #   url = "github:hyprwm/hyprpaper";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     disko = {
       url = "github:nix-community/disko";
