0787be28aa3815526108aa77f112998af0a51d50
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-06-09 12:18:26 +0200

    update plex

--- hosts/server/media-services.nix
+++ hosts/server/media-services.nix
@@ -67,10 +67,10 @@
       let
         plexLatest = pkgs.plex.override {
           plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-            version = "1.41.7.9799-5bce000f7";
+            version = "1.41.8.9834-071366d65";
             src = pkgs.fetchurl {
               url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-              sha256 = "sha256-Jp1Ji8BeCc0rGbADVH4SfzdeKHVpoHttEgIO3f5jRtw=";
+              sha256 = "sha256-fhm61vxJOWba2ngLzHCssqSCgO9JG7zurBJ90fSnAS4=";
             };
           });
         };
