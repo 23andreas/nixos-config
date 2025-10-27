d8f38658591334e2a1e00e206194834584d5a80d
Andreas Skønberg <andreas.skonberg@gmail.com>
2025-10-27 09:44:39 +0100

    update catppuccin config

--- modules/home-manager/applications/terminal/neovim/nvim/lua/plugins/catppuccin.lua
+++ modules/home-manager/applications/terminal/neovim/nvim/lua/plugins/catppuccin.lua
@@ -2,31 +2,38 @@ return {
   "catppuccin/nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-      no_italic = true,
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-      integrations = {
-        blink_cmp = true,
-        diffview = true,
-        gitsigns = true,
-        grug_far = true,
-        indent_blankline = {
-          enabled = true,
-          colored_indent_levels = false,
-        },
-        neotree = true,
-        neogit = true,
-        nvim_surround = true,
-        treesitter_context = true,
-        octo = true,
-        render_markdown = true,
-        telescope = true,
-        lsp_trouble = true,
-        which_key = true,
+  opts = {
+    transparent_background = true,
+    no_italic = true,
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    custom_highlights = function(colors)
+      return {
+        SnacksIndent = { fg = colors.surface0 },
+        SnacksIndentScope = { fg = colors.overlay2 }
       }
-    })
+    end,
+    integrations = {
+      blink_cmp = true,
+      diffview = true,
+      gitsigns = true,
+      grug_far = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = false,
+      },
+      neotree = true,
+      neogit = true,
+      nvim_surround = true,
+      treesitter_context = true,
+      octo = true,
+      render_markdown = true,
+      telescope = true,
+      lsp_trouble = true,
+      which_key = true,
+    }
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
   end,
 }
